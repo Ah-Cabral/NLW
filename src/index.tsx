a7308230b9e84e89270ec44cf55fc518bebe6f48
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-22 20:22:39 -0300

    Criando projeto React (Arquivos Base)

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+//Dentro do index html, não são colocados os elementos html diretamente,
+//o documento serve apenas como base
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
