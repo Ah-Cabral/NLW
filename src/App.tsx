ef143950b002a89def064fcdd0cdea06d211d520
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-22 21:31:03 -0300

    Adicionando comentários e inserindo componente button, que recebe 3 propriedades color.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { Button } from './components/Button'
+
+//Componentes São formas de conseguirmos isolar um pedaço de código
+//à fim de deixar o código mais legível. 
+//Também é uma forma reaproveitar partes de código.
 function App() {
   return (
-    <h1>Hello NLW</h1>
+    
+    //No react não podem-se ter
+    //diversos elementos juntos, se esses não estiverem dentro de outro.
+    //Nesse caso, os buttons estão dentro de uma div.
+
+    <div>
+      <Button color = 'red'/>
+      <Button color = 'blue'/>
+      <Button color = 'green'/>
+    </div>
+    
   );
 }
 
