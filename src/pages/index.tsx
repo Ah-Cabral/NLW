7fbe37534454c97b3965ab2cbada498bb4430454
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-24 09:58:01 -0300

    Inserindo Componente na index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Profile } from '../components/Profile'
 import styles from '../styles/components/Home.module.css'
 
 import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox'
 
 export default function Home() {
   return (
@@ -32,7 +33,7 @@ export default function Home() {
         </div>
 
         <div>
-          
+          <ChallengeBox />
         </div>
 
       </section>
